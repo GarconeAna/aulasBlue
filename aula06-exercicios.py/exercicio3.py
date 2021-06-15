4372f858405182bc581b516cb5fe2239c7e0c3ff
Ana Carolina <garcone.anaxx@gmail.com>
2021-06-15 17:20:56 -0300

    exercicio 3 completo

--- aula06-exercicios.py/exercicio3.py
+++ aula06-exercicios.py/exercicio3.py
@@ -1,3 +1,10 @@
 # 03 - Faça um script que o usuário escolha um número de início, um número de fim, e um número de
 # passo. O programa deve printar todos os números do intervalo entre início e fim, "pulando" de
 # acordo com o intervalo passado.
+
+numeroInicio = int(input('Digite o número inicial: '))
+numeroFim = int(input('Digite o número final: '))
+numeroIntervalo = int(input('Digite o número de intervalo entre os números digitados anteriormente: '))
+
+for i in range(numeroInicio,numeroFim,numeroIntervalo) :
+    print(i)

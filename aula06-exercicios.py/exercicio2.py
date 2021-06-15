c5e4d8716d053d5e53bcfee09883b30336b129b7
Ana Carolina <garcone.anaxx@gmail.com>
2021-06-15 17:15:17 -0300

    exercicio 2 resolvido

--- aula06-exercicios.py/exercicio2.py
+++ aula06-exercicios.py/exercicio2.py
@@ -1,1 +1,16 @@
 # 02 - Desenvolva um código que pergunte um número n para o usuário e exiba todos seus divisores
+
+numero = int(input('Digite um número: '))
+
+for i in range(numero) :
+    if i < numero :
+        i+=1
+        modulo = numero % i
+        if modulo == 0 :
+            print(f'{i} é divisor de {numero}')
+
+
+# for i in range(1,numero) :
+#     if numero % i == 0 :
+#         print(i)
+

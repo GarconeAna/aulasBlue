2d7f0c8e9c994034ecb34b53266c0169a80fad83
Ana Carolina <garcone.anaxx@gmail.com>
2021-06-15 16:33:52 -0300

    concluido o exercicio 1

--- aula06-exercicios.py/exercicio1.py
+++ aula06-exercicios.py/exercicio1.py
@@ -1,2 +1,31 @@
 # 01 - Dado uma string com uma frase informada pelo usuário (incluindo espaços em branco), conte
 # quantas vezes aparece as vogais a,e,i,o,u
+
+frase =input('Digite um frase: ')
+vogalA = 0 
+vogalE = 0
+vogalI = 0
+vogalO = 0
+vogalU = 0
+
+
+for i in frase :
+    if i == 'a' :
+        vogalA += 1
+    if i == 'e' :
+        vogalE += 1
+    if i == 'i' :
+        vogalI += 1
+    if i == 'o' :
+        vogalO += 1
+    if i == 'u' :
+        vogalU += 1
+    
+print(f'A frase digitada tem {vogalA} vogal(is) "a".')
+print(f'A frase digitada tem {vogalE} vogal(is) "e".')
+print(f'A frase digitada tem {vogalI} vogal(is) "i".')
+print(f'A frase digitada tem {vogalO} vogal(is) "o".')
+print(f'A frase digitada tem {vogalU} vogal(is) "u".')
+    
+
+ 

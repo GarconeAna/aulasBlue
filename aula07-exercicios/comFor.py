db6f5afe7809618332c1c83139b3b22f879e46a4
Ana Carolina <garcone.anaxx@gmail.com>
2021-06-16 16:04:05 -0300

    modificações

---
+++ aula07-exercicios/comFor.py
@@ -0,0 +1,11 @@
+# Execute as atividades abaixo, utilizando a estrutura de repetição FOR:
+####01 - Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi
+# o maior e o menor peso lidos.
+####02 - Crie um programa que pergunte ao usuário um número inteiro e faça a
+# tabuada desse número.
+####03 - Crie um programa que leia o ano de nascimento de sete pessoas. No final,
+# mostre quantas pessoas ainda não atingiram a maioridade e quantas já são
+# maiores.
+####04 - Desenvolva um programa que leia seis números inteiros e mostre a soma
+# apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+# Mostre também quantos valores pares foram digitados.
---
+++ aula07-exercicios/comWhile.py
@@ -0,0 +1,20 @@
+# Execute as atividades abaixo, utilizando a estrutura de repetição WHILE:
+####01 - Crie um programa que leia dois valores e mostre um menu na tela:
+# [ 1 ] somar
+# [ 2 ] multiplicar
+# [ 3 ] maior
+# [ 4 ] novos números
+# [ 5 ] sair do programa
+# Seu programa deverá realizar a operação solicitada em cada caso. (utilizar while
+# sem break)
+####02 - Crie um programa que leia a idade e o sexo de várias pessoas. A cada
+# pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não
+# continuar. No final, mostre:
+# A) Quantas pessoas têm mais de 18 anos.
+# B) Quantos homens foram cadastrados.
+# C) Quantas mulheres têm menos de 20 anos.
+####03 - Crie um programa que leia o nome e o preço de vários produtos. O programa
+# deverá perguntar se o usuário vai continuar ou não. No final, mostre:
+# A) Qual é o total gasto na compra.
+# B) Quantos produtos custam mais de R$1000.
+# (C) Qual é o nome do produto mais barato.

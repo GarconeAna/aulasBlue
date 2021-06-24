de2983b3b10534fa2ea63cc3a03983d6bc6e6add
Ana Carolina <garcone.anaxx@gmail.com>
2021-06-24 00:36:34 -0300

    aprendendo funções

---
+++ aula12-exercicios/maisExercicios.py
@@ -0,0 +1,27 @@
+#### Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastreos 
+# (com idade) em um dicionário. Se por acaso a CTPS for diferente de 0, o dicionário
+# receberá também o ano de contratação e o salário. Calcule e acrescente , além da idade
+# , com quantos anos a pessoa vai se aposentar. Considere que o trabalhador deve
+# contribuir por 35 anos para se aposentar.
+
+
+
+
+
+#### DESAFIO: Crie um programa que leia nome, sexo biologico e idade de várias pessoas,
+# guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma
+# lista. No final, mostre:
+# A) Quantas pessoas estão cadastradas.
+# B) A média da idade.
+# C) Uma lista com as mulheres.
+# D) Uma lista com as idades que estão acima da média.
+# OBS: O programa deve garantir que o sexo digitado seja válido, e que quando
+# perguntar ao usuário se deseja continuar a resposta seja somente sim ou não.
+
+
+
+#### Desafio: Continuando o exercício 3 crie agora um boletim escolar, seu programa deve 
+# receber 5 notas de 15 alunos, assim como o nome desses alunos, o programa tem que
+# calcular a média desse aluno e mostrar a situação dele, seguindo os mesmos critérios
+# apresentados no exercício 3. No final apresente todos nomes, as 5 notas, as médias e as
+# situações dos 15 alunos de uma vez só.
---
+++ aula13-exercicios/funcoes.py
@@ -0,0 +1,7 @@
+def area(l,c) :
+    areaTerreno = l * c
+    print(f'A area do terreno é {areaTerreno}m²')
+
+largura = float(input('Qual é a largura do terreno? '))
+comprimento = float(input('Qual é o comprimento do terreno? '))
+area(largura,comprimento)

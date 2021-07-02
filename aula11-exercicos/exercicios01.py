aa2437e37acea39327eb93c2d501c61c1372a20d
Ana Carolina <garcone.anaxx@gmail.com>
2021-07-02 15:33:20 -0300

    exercicio feito pelo professor, pois não conseguir fazer

--- aula11-exercicos/exercicios01.py
+++ aula11-exercicos/exercicios01.py
@@ -61,14 +61,15 @@
 
 # correção do exercicio 03 pelo professor
 
-itensComprados = []
-totalQuantidadeGeral = []
 
-estoque = {'coca':15, 'chocolate':5, 'batata':11, 'papel':3, 'presunto':26}
+itens_comprados = []
+total_quantidade_geral = 0
 
-continuar = input('Bem vindo ao Supermercado T3C5. Deseja ir as commpras? ')
-while continuar not in ['s','n'] :
-    continuar = input('Resposta inválida. Deseja ir nas compras (s/n) ').lower()
+estoque = {'coca':15, 'chocolate':6, 'batata':11, 'papel':3, 'presunto':26}
+
+continuar = input('Bem-vindo(a) ao Supermercado T3C5!. Deseja ir as compras? (s/n)').lower()
+while continuar not in ['s','n']:
+    continuar = input('Resposta inválida. Deseja ir as compras (s/n)').lower()
 
 while continuar == 's':
     print()
@@ -80,8 +81,29 @@ while continuar == 's':
     
     print()
 
-    produto = input
+    produto = input('Qual produto vc deseja comprar?')
+    quantidade_atual = estoque.get(produto,-1)
 
-    continuar = input('Deseja continuar as compras? (s/n) ').lower()
-    while continuar not in ['s','n'] :
-        continuar = input('Resposta inválida. Deseja ir nas compras (s/n) ').lower()
+    if quantidade_atual == -1:
+        print('Produto Inválido​')
+    elif quantidade_atual == 0:
+        print('Produto Indisponível​')
+    else:
+        quantidade = int(input('Qual a quantidade desejada?'))
+        if quantidade > quantidade_atual:
+            print(f'Quantidade solicitada não disponível. No momento temos apenas a quantidade de {quantidade_atual} em estoque')
+        else:
+            estoque[produto] = quantidade_atual - quantidade
+            if produto not in itens_comprados:
+                itens_comprados.append(produto)
+            total_quantidade_geral += quantidade
+            print('Compra realizado com sucesso!!!')
+
+    continuar =  input('Deseja continuar as compras? (s/n)').lower()
+    while continuar not in ['s','n']:
+        continuar = input('Resposta inválida. Deseja ir as compras (s/n)').lower()
+
+print()
+print('---Resumo da compra---')
+print(f'Quantidade de itens comprados: {len(itens_comprados)}')
+print(f'Total de itens comprados: {total_quantidade_geral}')
